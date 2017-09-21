de6e3666ddae75aecb8f2dcae197567253d36b7b
Tim Whitaker <camp4climber@gmail.com>
2017-09-21 17:02:18 -0600

    Add drawing to our canvas element

---
+++ app/static/app.js
@@ -0,0 +1,33 @@
+var frame = getComputedStyle(document.getElementById('frame'));
+var canvas = document.getElementById('paint');
+var ctx = canvas.getContext('2d');
+var mouse = {x:0, y:0};
+
+canvas.width = parseInt(frame.getPropertyValue('width'));
+canvas.height = canvas.width / 2;
+
+ctx.lineWidth = 3;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.strokeStyle = '#000000';
+
+canvas.addEventListener('mousemove', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+}, false);
+
+canvas.addEventListener('mousedown', function(e) {
+  ctx.beginPath();
+  ctx.moveTo(mouse.x, mouse.y);
+
+  canvas.addEventListener('mousemove', paint, false);
+}, false);
+
+canvas.addEventListener('mouseup', function() {
+  canvas.removeEventListener('mousemove', paint, false);
+}, false);
+
+var paint = function() {
+  ctx.lineTo(mouse.x, mouse.y);
+  ctx.stroke();
+};
